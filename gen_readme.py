5badefa92b6ad5b0fe9cb73a28596bbfb5138632
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-26 15:47:03 +0100

    gen_readme: add RSS badge

--- gen_readme.py
+++ gen_readme.py
@@ -29,6 +29,8 @@ Backup of my blog posts in https://kokada.capivaras.dev/.
 
 ## Posts
 
+[![RSS](https://img.shields.io/badge/RSS-FFA562?style=for-the-badge&logo=rss&logoColor=white)](https://raw.githubusercontent.com/thiagokokada/blog/main/rss.xml)
+
 {posts}\
 """
 RSS_TITLE = "kokada's blog"
