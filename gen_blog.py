25a0513962a60d1dfdfbfd25e9a963ab7431ad27
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-26 15:57:17 +0100

    gen_blog: rename from gen_readme, add license

--- gen_blog.py
+++ gen_blog.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2024 Thiago Kenji Okada <thiagokokada@gmail.com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 """\
-Usage: gen_readme.py
+Usage: gen_blog.py
 
 Needs to be run at the root directory of the repository.
 
 Will output the generated README.md file to the stdout, you can redirect its
 contents by:
 
-  $ ./gen_readme.py > README.md
+  $ ./gen_blog.py > README.md
 
 Will also generate the RSS file in `rss.xml`.\
 """
